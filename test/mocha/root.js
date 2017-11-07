6f443719512ae26e47d83770ed3ed8d14017c4b5
benho_kiostech <benho314@gmail.com>
2017-11-07 19:31:27 +0800

    1. Initiate mocha test script

---
+++ test/mocha/root.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var should = require('chai').should();
+var supertest = require('supertest')
+var api = supertest.agent('localhost:3000');
+var server = require('../../server/server.js');
+
+//before(function() {
+	//server.start();
+//});
+
+//after(function(){
+		////server.stop();
+		////
+//});
+
+describe('API root access Test', function() {
+	it("should return status 200",function(done){
+		api
+			.get("/")
+			.expect("Content-type",/json/)
+			.expect(200)
+			.end(function(err,res){
+				res.status.should.equal(200);
+				done();
+			});			  
+	});
+});
+
