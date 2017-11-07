feba9812df336f9dd78edd7e15bd20bf57783e89
benho_kiostech <benho314@gmail.com>
2017-11-07 18:48:28 +0800

    1. Add Docker Setup for local development 2. Configure datasource setup

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:6.11.1
+
+# Create App Directory
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+# Install App Dependencies
+COPY package.json /usr/src/app
+RUN npm install
+
+# Bundle App Source Code
+COPY . /usr/src/app
+
+EXPOSE 3000
+CMD ["npm","run","start"]
---
+++ server/datasources.development.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+	mongodb: {
+		connector: 'loopback-connector-mongodb',
+		hostname: process.env.KT_VAR_MONGODB_HOST || 'localhost',
+	  port: process.env.KT_VAR_MONGODB_PORT || 27017,
+		user: process.env.KT_VAR_MONGODB_USERNAME || 'teleClawAdmin',
+		password: process.env.KT_VAR_MONGODB_PASSWORD || '123456',
+		database: process.env.KT_VAR_MONGODB_DATABASE || 'teleclaw_dev',
+		lazyConnect : true
+	}
+};
+
