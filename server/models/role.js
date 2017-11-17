2cdced308b65571baf9847593ffeb7ce13ad29c4
Chris Choy <choylapchichris@gmail.com>
2017-11-17 15:49:14 +0800

    create data models

---
+++ server/models/role.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = (Role) => {
+  //Check whether database exist default user role, if no , create them
+  Role.on('attached', () => {
+    let createdTime = new Date().getTime();
+    let defaultUserRoles = [
+      { name : "user" , description : "People to login in to our apps, who can perform view, play, charge, report issue, etc" , created : createdTime},
+      { name : "teleClawAdmin" , description : "Administrators / operaters of TeleClaw, who can access the dashboard and perform admin actions" , created : createdTime},
+    ];
+    defaultUserRoles.map((role) => {
+      Role.find({ where : { name : role.name } }, (err,data)=>{
+        if(data !== undefined && data.length == 0){
+          Role.create(role);
+        }
+      });
+    });
+  });
+};
