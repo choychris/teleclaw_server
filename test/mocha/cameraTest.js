3f6f3ce75d279995757b65af9dfcb930dab87335
Chris Choy <choylapchichris@gmail.com>
2017-12-10 17:17:40 +0800

    add machine relations

--- test/mocha/cameraTest.js
+++ test/mocha/cameraTest.js
@@ -13,42 +13,42 @@ const generateJSONAPI = (url, filter) => {
 
 describe('Attach a related models to product', function(){
 
-  //  describe('Create Camera', function(){
-  //   it('should return camera object', function(done){
-  //     var api = supertest.agent(baseUrl);
-  //     var cameraBody = { 
-  //       name: "camera1",
-  //       position: 'front',
-  //       type: 'main',
-  //       webrtcServer: 'http://webrtcstreamer-env.ap-southeast-1.elasticbeanstalk.com/',
-  //       rtspDdnsUrl: 'rtsp://188773sc14.iask.in:554/live/sub',
-  //       localIp: '192.168.2.101',
-  //       alibabaSetting: {
-  //         domain: 'live3.teleclaw.win',
-  //         appName: 'teleclaw_dev',
-  //         streamName: 'camera1',
-  //         rtmp: "rtmp://live3.teleclaw.win/teleclaw_dev/camera1?auth_key=1512806440-0-0-c1161f52d3dce54069b16d9436fb5eab",
-  //         flv: "http://live3.teleclaw.win/teleclaw_dev/camera1.flv?auth_key=1512806440-0-0-7ee97130c8412574c977a895750046f1",
-  //         m3u8: "http://live3.teleclaw.win/teleclaw_dev/camera1.m3u8?auth_key=1512806440-0-0-6cbb043751a4d055c683228cd960cabf"
-  //       }
-  //     }
+   describe('Create Camera', function(){
+    it('should return camera object', function(done){
+      var api = supertest.agent(baseUrl);
+      var cameraBody = { 
+        name: "camera1",
+        position: 'front',
+        type: 'main',
+        webrtcServer: 'http://webrtcstreamer-env.ap-southeast-1.elasticbeanstalk.com/',
+        rtspDdnsUrl: 'rtsp://188773sc14.iask.in:554/live/sub',
+        localIp: '192.168.2.101',
+        alibabaSetting: {
+          domain: 'live3.teleclaw.win',
+          appName: 'teleclaw_dev',
+          streamName: 'camera1',
+          rtmp: "rtmp://live3.teleclaw.win/teleclaw_dev/camera1?auth_key=1512806440-0-0-c1161f52d3dce54069b16d9436fb5eab",
+          flv: "http://live3.teleclaw.win/teleclaw_dev/camera1.flv?auth_key=1512806440-0-0-7ee97130c8412574c977a895750046f1",
+          m3u8: "http://live3.teleclaw.win/teleclaw_dev/camera1.m3u8?auth_key=1512806440-0-0-6cbb043751a4d055c683228cd960cabf"
+        }
+      }
 
-  //     api
-  //       .post('/api/cameras')
-  //       .send(cameraBody)
-  //       .set('Accept', 'application/json')
-  //       .end(function(err,res){
-  //         // console.log(res.body);
-  //         global.Camera = res.body;
-  //         res.body.should.be.an('object');
-  //         res.status.should.equal(200);
-  //         done();
-  //       });
-  //   });
-  // });
+      api
+        .post('/api/cameras')
+        .send(cameraBody)
+        .set('Accept', 'application/json')
+        .end(function(err,res){
+          // console.log(res.body);
+          global.Camera = res.body;
+          res.body.should.be.an('object');
+          res.status.should.equal(200);
+          done();
+        });
+    });
+  });
 
   describe('Select a machine and get related info', function(){
-    it('should return machine object', function(done){
+    it('should return machine array', function(done){
       var api = supertest.agent(baseUrl);
       //let machineId = 'f0348d84-a1ae-48c5-ab9a-bdd45cb54759';
       let url = `/api/machines?access_token=${accessToken}`;
@@ -59,7 +59,6 @@ describe('Attach a related models to product', function(){
         .get(generateJSONAPI(url, filter))
         .set('Accept', 'application/json')
         .end(function(err,res){
-          console.log(res.body);
           global.machineInfo = res.body[0];
           res.body.should.be.an('array');
           res.status.should.equal(200);
@@ -68,22 +67,42 @@ describe('Attach a related models to product', function(){
     });
   });
 
-  // describe('Attach the machineId to the cammera', function(){
-  //   it('should return camera object', function(done){
-  //     var api = supertest.agent(baseUrl);
-  //     //let machineId = 'f0348d84-a1ae-48c5-ab9a-bdd45cb54759';
-  //     let url = `/api/cameras/${global.Camera.id}?access_token=${accessToken}`;
-  //     api
-  //       .patch(url)
-  //       .set('Accept', 'application/json')
-  //       .send({machineId: global.machineInfo.id})
-  //       .end(function(err,res){
-  //         res.body.should.be.an('object');
-  //         res.status.should.equal(200);
-  //         done();
-  //       });
-  //   });
-  // });
+  describe('Attach the machineId to the cammera', function(){
+    it('should return camera object', function(done){
+      var api = supertest.agent(baseUrl);
+      //let machineId = 'f0348d84-a1ae-48c5-ab9a-bdd45cb54759';
+      let url = `/api/cameras/${global.Camera.id}?access_token=${accessToken}`;
+      api
+        .patch(url)
+        .set('Accept', 'application/json')
+        .send({machineId: global.machineInfo.id})
+        .end(function(err,res){
+          res.body.should.be.an('object');
+          res.status.should.equal(200);
+          done();
+        });
+    });
+  });
+
+  describe('Get machine camera info', function(){
+    it('should return machine object with camera', function(done){
+      var api = supertest.agent(baseUrl);
+      //let machineId = 'f0348d84-a1ae-48c5-ab9a-bdd45cb54759';
+      let url = `/api/machines?access_token=${accessToken}`;
+      let filter = {
+        include: 'cameras'
+      }
+      api
+        .get(generateJSONAPI(url, filter))
+        .set('Accept', 'application/json')
+        .end(function(err,res){
+          res.body.should.be.an('array');
+          res.status.should.equal(200);
+          console.log(res.body[0]);
+          done();
+        });
+    });
+  });
 
   // describe('change the machine iotPlatform info', function(){
   //   it('should return machine object', function(done){
