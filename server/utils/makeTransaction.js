596949fedeb81a60679427d0498777ef25240aea
Chris Choy <choylapchichris@gmail.com>
2017-12-11 18:55:01 +0800

    fix bugs

--- server/utils/makeTransaction.js
+++ server/utils/makeTransaction.js
@@ -14,6 +14,7 @@ export function makeTransaction(model, modelId, modelAttribute, amount, plusOrMi
       });
     }else if(plusOrMinus === 'plus'){
       let newNumber = parsedModel[modelAttribute] + amount;
+      let roundNumber = Math.round(newNumber);
        foundModel.updateAttributes({[modelAttribute]: roundNumber}, (err, instance)=>{
         if(err){
           next(err);
