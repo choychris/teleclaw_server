b393d7a2e0cc36c5dda8585a78d6db39401aa2b1
Chris Choy <choylapchichris@gmail.com>
2017-12-13 16:14:44 +0800

    fix db url

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -3,6 +3,6 @@
 module.exports = {
   mongodb: {
     connector: 'loopback-connector-mongodb',
-    url: process.env.KT_VAR_MONGODB_URL || 'mongo://teleClawAdmin:123456@localhost:27017/teleclaw_dev',
+    url: process.env.KT_VAR_MONGODB_URL || 'mongodb://teleClawAdmin:123456@localhost:27017/teleclaw_dev',
   },
 };
