92093ab4b4bf323f28e0f78a0cf16c3a444e48b6
Chris Choy <choylapchichris@gmail.com>
2017-12-14 12:33:39 +0800

    start test

--- test/mocha/merge.js
+++ test/mocha/merge.js
@@ -4,10 +4,12 @@ if(process.env.NODE_ENV === 'staging'){
   var server = require('../../build/server.js');
 
   before(function() {
+    console.log('server start');
     server.start();
   });
 
   after(function(){
+    console.log('server stop');
     server.stop();  
   });
 }
