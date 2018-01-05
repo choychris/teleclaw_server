e84bc39582fb3580f707f31a57d6939e6b8c0742
Chris Choy <choylapchichris@gmail.com>
2018-01-05 20:30:52 +0800

    email noti

--- server/models/issue.js
+++ server/models/issue.js
@@ -2,6 +2,7 @@
 
 import { updateTimeStamp, assignKey } from '../utils/beforeSave.js';
 import { loggingModel } from '../utils/createLogging.js';
+let { GMAIL_ADDRESS } = process.env;
 
 module.exports = function(Issue) {
 
@@ -27,6 +28,33 @@ module.exports = function(Issue) {
         })
       }
       next();
+    }else{
+      next();
+    }
+  })
+
+  Issue.observe('after save', (ctx, next)=>{
+    if(ctx.isNewInstance){
+      let { Email } = app.models;
+      let { type, email, message, userId, machineId, deliveryId, transactionId } = ctx.instance; 
+      Email.send({
+        to: `${GMAIL_ADDRESS}`,
+        from: `${GMAIL_ADDRESS}`,
+        subject: `Issue report from users : ${type}`,
+        html: `<h1>${message}</h1>
+          <h2>userId: ${userId}</h2>
+          <h2>User email: ${email}</h2>
+          <h2>machineId: ${machineId}</h2>
+          <h2>deliveryId: ${deliveryId}</h2>
+          <h2>transactionId: ${transactionId}</h2>`
+      }, function(err, mail){
+        if(err){
+          console.log('error in sending mail : ', err)
+          next(err)
+        }else{
+          next();
+        }
+      })
     }else{
       next();
     }
