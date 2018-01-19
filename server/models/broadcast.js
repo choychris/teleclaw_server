53c6e0c82d0341af37cdf95655b9c1caf78d67c4
Chris Choy <choylapchichris@gmail.com>
2018-01-19 15:53:58 +0800

    fix bug

--- server/models/broadcast.js
+++ server/models/broadcast.js
@@ -4,8 +4,8 @@ import { updateTimeStamp, assignKey } from '../utils/beforeSave.js';
 
 module.exports = function(Broadcast) {
   //assgin an id to each newly created model
-  assignKey(Benchmark);
+  assignKey(Broadcast);
 
   // assgin last updated time / created time to model
-  updateTimeStamp(Benchmark);
+  updateTimeStamp(Broadcast);
 };
