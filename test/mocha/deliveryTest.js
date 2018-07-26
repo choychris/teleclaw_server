9fe40b93a765bfe3f4fe9e53c142c6169abacdb7
ChrisChoy <choylapchichris@gmail.com>
2018-07-26 19:49:31 +0800

    change delivery for prize

--- test/mocha/deliveryTest.js
+++ test/mocha/deliveryTest.js
@@ -16,41 +16,44 @@ if(NODE_ENV == 'staging' || NODE_ENV == 'production'){
   });
 }
 
+global.accessToken = 'z7XVvlIbDhSZG649KU59bljIyv9xWAdTOBpVIK0ZHK2HlNOKIR3S5n5wJHCRzJVY';
+global.lbUserId ='5b23939f29e05d00bd846546';
+
 const generateJSONAPI = (url, filter) => {
   return url + '&filter=' + JSON.stringify(filter) ;
 }
 
 // |================== Authenticate User API ==================|
-describe('Login / Create User first', function(){
-  it('login / create current user - status 200 and token', function(done){
-  var api = supertest.agent(baseUrl);
-  var userInfo = {
-    prvoider: 'facebook',
-    accessToken : 'EAACDHoPDoIMBAMDWVuWrysgH2d6MtLxdSuiZCxxJTNf9ZBEEFL3uPgDSWxoSHzRQv4G1eYzFc2p3XT6eZCQ1g7bLI8ZCFe2ZCmbqNtlnZAXpppcSWS525yXCMINzFaGLki5ZA3hJ0QVjp4519HjH5ghxAw2pXLSyqMKLEAsbrpHSQZDZD',
-    username : 'Lap Chi',
-    expiresIn: 5173511,
-    userId:  "10156122556431165",
-    picture: {
-      height: 100,
-      is_silhouette: false,
-      url: "https://scontent.xx.fbcdn.net/v/t1.0-1/p100x100/1916279_10154397272841165_6485132739615337980_n.jpg?oh=838585186d56fc60e4dcfa90aa9ee10e&oe=5A8E8B2F",
-      width: 100
-    }
-  }
+// describe('Login / Create User first', function(){
+//   it('login / create current user - status 200 and token', function(done){
+//   var api = supertest.agent(baseUrl);
+//   var userInfo = {
+//     prvoider: 'facebook',
+//     accessToken : 'EAACDHoPDoIMBAMDWVuWrysgH2d6MtLxdSuiZCxxJTNf9ZBEEFL3uPgDSWxoSHzRQv4G1eYzFc2p3XT6eZCQ1g7bLI8ZCFe2ZCmbqNtlnZAXpppcSWS525yXCMINzFaGLki5ZA3hJ0QVjp4519HjH5ghxAw2pXLSyqMKLEAsbrpHSQZDZD',
+//     username : 'Lap Chi',
+//     expiresIn: 5173511,
+//     userId:  "10156122556431165",
+//     picture: {
+//       height: 100,
+//       is_silhouette: false,
+//       url: "https://scontent.xx.fbcdn.net/v/t1.0-1/p100x100/1916279_10154397272841165_6485132739615337980_n.jpg?oh=838585186d56fc60e4dcfa90aa9ee10e&oe=5A8E8B2F",
+//       width: 100
+//     }
+//   }
 
-  api
-    .post(`/api/users/auth`)
-    .send(userInfo)
-    .set('Accept', 'application/json')
-    .end(function(err,res){
-        global.accessToken = res.body.result.lbToken.id;
-        global.lbUserId = res.body.result.lbToken.userId;
-        res.body.result.should.be.an('object');
-        res.status.should.equal(200);
-        done();
-     });
-  });
-});
+//   api
+//     .post(`/api/users/auth`)
+//     .send(userInfo)
+//     .set('Accept', 'application/json')
+//     .end(function(err,res){
+//         global.accessToken = res.body.result.lbToken.id;
+//         global.lbUserId = res.body.result.lbToken.userId;
+//         res.body.result.should.be.an('object');
+//         res.status.should.equal(200);
+//         done();
+//      });
+//   });
+// });
 
 // |================ GET Product List ================|
 // describe('Find product list', function(){
@@ -77,7 +80,10 @@ describe('Login / Create User first', function(){
 describe('Get delivery rate quote', function(){
   this.timeout(4000);
   it('should return the list of courier rate', function(done){
-  global.productIds = [{"id":'ae55c637-26a8-45f6-9fb0-fe716bcfe176', playId:'BkSMizqzM'}, {id: 'ae55c637-26a8-45f6-9fb0-fe716bcfe176', playId: 'ByE0nW5fG'}]
+  global.productIds = [
+    {"id":'ae55c637-26a8-45f6-9fb0-fe716bcfe176', playId:'BkSMizqzM'},
+    {"id": 'ae55c637-26a8-45f6-9fb0-fe716bcfe176', playId: 'ByE0nW5fG'},
+  ]
   var api = supertest.agent(baseUrl);
   var data = {
     products: global.productIds,
